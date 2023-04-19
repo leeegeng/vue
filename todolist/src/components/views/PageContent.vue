f4cad62c3aa726d84b5997b91b18849f5fcb8ca6
leeegeng <ligeng04@126.com>
2023-04-19 15:29:30 +0800

    add scss compile

--- todolist/src/components/views/PageContent.vue
+++ todolist/src/components/views/PageContent.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="outer">
     <div class="page-content">
-      <div class="box" v-for="item in layout" :key="item.key">
-        <div class="box-header">
-          <span class="titile">{{ item.cnName }}</span>
-          <div class="control">
-            <span class="refresh">刷新</span>
-            <span class="fullscreen">全屏</span>
+      <template v-for="item in layout">
+        <div
+          :class="['box', item.isFullScreen ? 'fullscreen' : '']"
+          v-show="!isFullScreen || (isFullScreen && item.isFullScreen)"
+        >
+          <div class="box-header">
+            <span class="titile">{{ item.cnName }}</span>
+            <div class="control">
+              <span class="refresh">刷新</span>
+              <span class="fullscreen" @click="fullScreen(item)">{{
+                item.isFullScreen ? '取消全屏' : '全屏'
+              }}</span>
+            </div>
           </div>
+          <div class="box-content"></div>
         </div>
-        <div class="box-content"></div>
-      </div>
-
+      </template>
       <i class="default" v-for="i in layout.length" :key="i"></i>
     </div>
   </div>
@@ -25,7 +31,24 @@ export default {
   data() {
     return {
       layout: [],
+      isFullScreen: false,
     }
+  },
+  methods: {
+    fullScreen(item) {
+      this.isFullScreen = !this.isFullScreen
+
+      let that = this
+
+      this.layout.forEach((comp) => {
+        if (comp.key === item.key) {
+          item.isFullScreen = that.isFullScreen
+          comp.isFullScreen = that.isFullScreen
+        } else {
+          comp.isFullScreen = false
+        }
+      })
+    },
   },
   mounted() {
     axios
@@ -41,7 +64,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .outer {
   display: block;
   width: 100%;
@@ -56,30 +79,38 @@ export default {
   /* align-content: flex-end; */
   box-sizing: border-box;
   align-items: flex-start;
-}
-.page-content .box {
-  width: 310px;
-  height: 300px;
-  background-color: whitesmoke;
-  border: 1px solid black;
-  margin: 1px;
-  box-sizing: border-box;
-  align-self: flex-start;
-}
+  .box {
+    width: 310px;
+    height: 300px;
+    background-color: whitesmoke;
+    border: 1px solid black;
+    margin: 1px;
+    box-sizing: border-box;
+    align-self: flex-start;
 
-.default {
-  width: 310px;
-  height: 0;
-}
+    .box-header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      span {
+        padding: 0 6px;
+      }
 
-.page-content .box .box-header {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
+      .control:hover {
+        cursor: pointer;
+      }
+    }
+  }
 
-.page-content .box .box-header .control:hover {
-  cursor: pointer;
+  .fullscreen {
+    width: 100%;
+    height: 600px;
+  }
+
+  .default {
+    width: 310px;
+    height: 0;
+  }
 }
 </style>
